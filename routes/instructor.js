73ac47f39ed8ff2913422019940e5374d92a028c
CuSinhVip0 <sinhnguyen2k2@gmail.com>
2026-02-08 12:22:33 +0700

    feat: add lesson management routes for assigning, updating, and deleting lessons

--- routes/instructor.js
+++ routes/instructor.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getDb } = require('../config/firebase');
 const { sendEmail, sendEmailCreateStudent } = require('../config/email');
-
 const uuidv4 = require('uuid').v4;
+const otpGenerator = require('otp-generator')
 
 /**
  * POST /addStudent
@@ -211,6 +211,221 @@ router.delete('/student/:phone', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Failed to delete student', details: error.message });
     }
+});
+
+
+/***
+ * POST /assignLesson
+ * Params: studentPhone, title, description
+ * Action: Save under student’s lesson list
+ */
+router.post('/assignLesson', async (req, res) => {
+    try {
+        const { studentPhone, title, description } = req.body;
+        if (!studentPhone || !title || !description) {
+            return res.status(400).json({ error: 'Student phone, title, description are required' });
+        }
+
+        const db = getDb();
+
+        const studentFB = await db.collection('users')
+            .where('phone', 'in', studentPhone)
+            .where('role', '==', 'student')
+            .get();
+
+        if (studentFB.empty) {
+            return res.status(404).json({ error: 'No students found' });
+        }
+
+        const lessionID = otpGenerator.generate(8, { upperCaseAlphabets: false, specialChars: false });
+        const lesson = {
+            id: `${lessionID}`,
+            title,
+            description,
+            assignedAt: new Date().toISOString(),
+            completed: false,
+            isActive: true,
+        };
+
+        await db.collection('lessons').doc(lessionID).set(lesson);
+
+        studentFB.docs.forEach(async doc => {
+            await db.collection('lesson_student').doc().set({
+                studentPhone: doc.data().phone,
+                lessonId: lessionID,
+                assignedAt: new Date().toISOString(),
+                completed: false
+            });
+        });
+
+        res.json({
+            success: true,
+            message: 'Lesson assigned successfully',
+            lesson
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to assign lesson', details: error.message });
+    }
+});
+
+router.post('/updateLesson', async (req, res) => {
+    try {
+        const { studentPhone, title, description, id } = req.body;
+        if (!Array.isArray(studentPhone) || !id) {
+            return res.status(400).json({ error: 'Student phone, id are required' });
+        }
+        const db = getDb();
+
+        const LessonFB = db.collection('lessons').doc(id)
+        const LessonID = await LessonFB.get();
+        if (!LessonID.exists) {
+            return res.status(404).json({ error: 'Lesson not found' });
+        }
+
+        // begin update
+        const updateData = {
+            updatedAt: new Date().toISOString()
+        };
+
+        if (title) updateData.title = title;
+        if (description) updateData.description = description;
+
+        await LessonFB.update(updateData);
+
+        const updatedDoc = await LessonFB.get();
+
+        const studentPhoneFB = await db.collection('lesson_student')
+            .where('studentPhone', 'in', studentPhone)
+            .get();
+
+        if (!studentPhoneFB.empty) {
+            const batch = db.batch();
+            studentPhoneFB.docs.forEach(doc => batch.delete(doc.ref));
+            await batch.commit();
+        }
+        const insertBatch = db.batch();
+
+        studentPhone.forEach(phone => {
+            const ref = db.collection('lesson_student').doc();
+            insertBatch.set(ref, {
+                studentPhone: phone,
+                lessonId: id,
+                assignedAt: new Date().toISOString(),
+                completed: false
+            });
+        });
+
+        await insertBatch.commit();
+
+        res.json({
+            success: true,
+            message: 'Lesson updated successfully',
+            lesson: {
+                ...updatedDoc.data()
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to assign lesson', details: error.message });
+    }
+});
+
+router.put('/editStatusLesson', async (req, res) => {
+    try {
+        const { id, isActive } = req.body;
+        const db = getDb();
+        const LessonFB = db.collection('lessons').doc(id)
+        const LessonID = await LessonFB.get();
+        if (!LessonID.exists) {
+            return res.status(404).json({ error: 'Lesson not found' });
+        }
+
+        // begin update
+        const updateData = {
+            updatedAt: new Date().toISOString()
+        };
+
+        if (typeof isActive === 'boolean') updateData.isActive = isActive;
+
+        await LessonFB.update(updateData);
+
+        res.json({
+            success: true,
+            message: 'Lesson updated successfully',
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update student', details: error.message });
+    }
+});
+router.delete('/lesson/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const db = getDb();
+        const lessonFB = db.collection('lessons').doc(id);
+        const lessonDoc = await lessonFB.get();
+
+        if (!lessonDoc.exists) {
+            return res.status(404).json({ error: 'Lesson not found' });
+        }
+
+        await lessonFB.delete();
+
+        const lessonStudentFB = await db.collection('lesson_student')
+            .where('lessonId', '==', id)
+            .get();
+
+        if (!lessonStudentFB.empty) {
+            const batch = db.batch();
+            lessonStudentFB.docs.forEach(doc => batch.delete(doc.ref));
+            await batch.commit();
+        }
+        res.json({
+            success: true,
+            message: 'Lesson deleted successfully'
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete lesson', details: error.message });
+    }
+});
+router.get('/lessons', async (req, res) => {
+    try {
+        const db = getDb();
+        const lessonsFB = await db.collection('lessons').get();
+
+        const lessons = await Promise.all(
+            lessonsFB.docs.map(async (doc) => {
+                const data = doc.data();
+
+                const lessonStudentFB = await db
+                    .collection('lesson_student')
+                    .where('lessonId', '==', data.id)
+                    .get();
+
+                const studentPhone = lessonStudentFB.docs.map(lsDoc => lsDoc.data().studentPhone);
+
+                return {
+                    id: data.id,
+                    title: data.title,
+                    description: data.description,
+                    assignedAt: data.assignedAt,
+                    completed: data.completed,
+                    isActive: data.isActive,
+                    createdAt: data.createdAt,
+                    studentPhone
+                };
+            })
+        );
+
+        res.json({
+            success: true,
+            lessons,
+            count: lessons.length
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to get lessons', details: error.message });
+    }
+
 });
 
 module.exports = router;
